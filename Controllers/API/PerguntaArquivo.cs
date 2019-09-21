c9b5f5b09e90ddef2968df98b790a70b515f0186
Lucas Zillig Barros Oliveira <l.zillig11@gmail.com>
2019-09-20 22:40:13 -0300

    Upload Arquivos

--- Controllers/API/PerguntaArquivo.cs
+++ Controllers/API/PerguntaArquivo.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PI_3.Models;
 
@@ -9,22 +13,26 @@ namespace PI_3.Controllers.API
 {   
     [Route("api/[controller]")]
     [ApiController] 
-    public class PerguntaArquivoController : ControllerBase
+    public class PerguntaArquivo : ControllerBase
     {
         public AppDbContext _context;
-        public PerguntaArquivoController (AppDbContext context)
+
+        private IHostingEnvironment _hostingEnvironment;
+
+        public PerguntaArquivo (AppDbContext context, IHostingEnvironment hostingEnvironment)
         {
             _context = context;
+            _hostingEnvironment = hostingEnvironment;
         }
-        
+
         [HttpGet]
-        public ActionResult<IEnumerable<PerguntaArquivo>> GetPerguntasArquivos()
+        public ActionResult<IEnumerable<Models.PerguntaArquivo>> GetPerguntasArquivos()
         {
             return _context.PerguntaArquivo.ToList();
         }
 
         [HttpGet("{id}")]
-        public ActionResult<PerguntaArquivo> GetPerguntaArquivo(int id)
+        public ActionResult<Models.PerguntaArquivo> GetPerguntaArquivo(int id)
         {
 
             var perguntaArquivo = _context.PerguntaArquivo.SingleOrDefault(i => i.PerguntaArquivoId == id);
@@ -33,7 +41,8 @@ namespace PI_3.Controllers.API
         }
 
         [HttpPost]
-        public ActionResult<PerguntaArquivo> AddPerguntaArquivo(PerguntaArquivo requestPerguntaArquivo)
+        [Route("[action]")]
+        public ActionResult<Models.PerguntaArquivo> AddPerguntaArquivo(Models.PerguntaArquivo requestPerguntaArquivo)
         {
             _context.PerguntaArquivo.Add(requestPerguntaArquivo);
 
@@ -42,8 +51,39 @@ namespace PI_3.Controllers.API
             return CreatedAtAction(nameof(GetPerguntaArquivo), new { id = requestPerguntaArquivo.PerguntaArquivoId }, requestPerguntaArquivo);
         }
 
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult UploadArquivo(IList<IFormFile> files)
+        {
+            foreach (IFormFile item in files)
+            {
+                string filename = ContentDispositionHeaderValue.Parse(item.ContentDisposition).FileName.Trim('"');
+                filename = this.EnsureFilename(filename);
+                using (FileStream filestream = System.IO.File.Create(this.GetPath(filename)))
+                {
+
+                }
+            }
+            return this.Content("Sucesso");
+        }
+
+        private string GetPath(string filename)
+        {
+            string path = _hostingEnvironment.WebRootPath + "\\upload\\";
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            return path + filename;
+        }
+
+        private string EnsureFilename(string filename)
+        {
+            if (filename.Contains("\\"))
+                filename = filename.Substring(filename.LastIndexOf("\\") + 1);
+            return filename;
+        }
+
         [HttpPut("{id}")]
-        public ActionResult UpdatePerguntaArquivo(int id, PerguntaArquivo requestPerguntaArquivo)
+        public ActionResult UpdatePerguntaArquivo(int id, Models.PerguntaArquivo requestPerguntaArquivo)
         {
             if (id != requestPerguntaArquivo.PerguntaArquivoId)
                 return BadRequest();
