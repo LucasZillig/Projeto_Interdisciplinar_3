4b4f565d2e4b1a4905e1094bebbc6511d13cae1d
rafapignataro <rafapignataro@gmail.com>
2019-09-24 16:36:23 -0300

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -66,7 +66,7 @@ namespace PI_3
             // app.UseAuthentication();
             //app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+            // app.UseCookiePolicy();
 
             app.UseMvc(routes =>
             {
